c94dc6d95ead8f8cbabe34c6b3702d5dc4aa5421
Anouar Et-Taleb <aet-tale@c2r6p7.1337.ma>
2024-04-29 12:54:18 +0100

    h

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: aet-tale <aet-tale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 14:11:54 by aet-tale          #+#    #+#             */
-/*   Updated: 2024/04/02 01:21:25 by aet-tale         ###   ########.fr       */
+/*   Updated: 2024/04/29 12:52:53 by aet-tale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ static void	ft_add_tostack(stack_arr **arr, char	**str)
 	while (str[i])
 	{
 		list_add_front(arr, ft_atoi(str[i]));
+		i++;
 	}
 }
-
 
 stack_arr	*duplicate_stack(stack_arr *arr)
 {
@@ -42,56 +42,57 @@ stack_arr	*duplicate_stack(stack_arr *arr)
 }
 
 
-void sort_stack(stack_arr *arr)
+// void sort_stack(stack_arr *arr)
+// {
+// 	int i = 0;
+// 	int tmp = 0;
+// 	stack_arr *tmp = arr;
+// 	int size = ft_lstsize(arr);
+
+
+// 	while (size > 1)
+// 	{
+// 		i = 0;
+// 		arr = tmp;
+// 		while (i +1 < size)
+// 		{
+// 			if (arr->nmbr > arr->next->nmbr)
+// 			{
+// 				tmp =  arr->nmbr;
+// 				arr->nmbr = arr->next;
+// 				arr->next->nmbr = tmp;
+// 			}
+// 			i++;
+// 		}
+// 		arr=arr->next;
+// 		size--;
+// 	}
+// }
+
+static void	free_linked(stack_arr *arr)
 {
-	int i = 0;
-	int tmp = 0;
-	stack_arr *tmp = arr;
-	int size = ft_lstsize(arr);
+	stack_arr	*tmp;
 
-
-	while (size > 1)
+	while (arr)
 	{
-		i = 0;
+		tmp = arr->next;
+		free(arr);
 		arr = tmp;
-		while (i +1 < size)
-		{
-			if (arr->nmbr > arr->next->nmbr)
-			{
-				tmp =  arr->nmbr;
-				arr->nmbr = arr->next;
-				arr->next->nmbr = tmp;
-			}
-			i++;
-		}
-		arr=arr->next;
-		size--;
 	}
 }
-// static void	free_linked(stack_arr *arr)
-// {
-// 	stack_arr	*tmp;
 
-// 	while (arr)
-// 	{
-// 		tmp = arr->next;
-// 		free(arr);
-// 		arr = tmp;
-// 	}
-// }
+static void	free_arr(char **str)
+{
+	int	i;
 
-// static void	free_arr(char **str)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	while (str[i])
-// 	{
-// 		free(str[i]);
-// 		i++;
-// 	}
-// 	free(str);
-// }
+	i = 0;
+	while (str[i])
+	{
+		free(str[i]);
+		i++;
+	}
+	free(str);
+}
 
 
 // void first_step(stack_arr *arr)
@@ -132,15 +133,16 @@ int	main(int ac, char **av)
 		{
 			str = ft_split(av[i]);
 			ft_add_tostack(&arr, str);
-			// free_arr(str);
+			free_arr(str);
 			// print_arr(duplicate_stack(arr));
 			// first_step(arr);
 			// str = NULL;
 			i++;
 		}
+		print_arr(arr);
+		free_linked(arr);
 	}
-	print_arr(arr);
-	// free_linked(arr);
+	// printf("%s", av[0]);
 	// system("valgrind ./push_swap");
 	// arr = NULL;
 }
